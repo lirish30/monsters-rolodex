42ffb1111e6c7fa0c313203bbe716c870f0f462a
Logan Irish <loganirish@Logans-MacBook-Pro-2.local>
2020-05-04 07:48:11 -0400

    Updates

---
+++ precache-manifest.d9f27e65ac5bf849a97421e9f68e84ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a739330e2a99419e925b92c46931e3f0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e4c8990fcef148b20859",
+    "url": "/monsters-rolodex/static/css/main.1d86e33b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c8990fcef148b20859",
+    "url": "/monsters-rolodex/static/js/main.2bad3240.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
